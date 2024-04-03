b83e5d696440212265dd36514812af6c51759734
mauricioatm <mauriciochasi@gmail.com>
2024-04-03 11:11:18 +0200

    aplicacion finalizada

--- fakeit/models.py
+++ fakeit/models.py
@@ -7,7 +7,3 @@ class Persona(models.Model):
     partido = models.CharField(max_length=100,
                                choices=[("Partido1", "Partido 1"), ("Partido2", "Partido 2"), ("Partido3", "Partido 3"),
                                         ("Partido4", "Partido 4")])
-
-class Resultados(models.Model):
-        persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
-        votos = models.IntegerField()
--- fakeit/views.py
+++ fakeit/views.py
@@ -6,8 +6,6 @@ from django.views import generic
 from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
-
-
 from fakeit.models import Persona
 
 
@@ -15,13 +13,13 @@ from fakeit.models import Persona
 class IndexView(generic.ListView):
     template_name = "graphic.html"
     context_object_name = "graph"
+
     def get_queryset(self):
         # Obtener un cursor para ejecutar consultas
         cursor = connection.cursor()
 
         # Consulta SQL para obtener los datos de la base de datos
-        cursor.execute("SELECT fakeit_persona.partido, COUNT(*) FROM fakeit_resultados "
-                       "JOIN fakeit_persona ON fakeit_resultados.persona_id = fakeit_persona.id "
+        cursor.execute("SELECT partido, COUNT(*) FROM fakeit_persona "
                        "GROUP BY fakeit_persona.partido")
 
         # Obtener los datos de la consulta
@@ -30,31 +28,44 @@ class IndexView(generic.ListView):
         # Cerrar el cursor
         cursor.close()
 
-        # Extraer los nombres de los partidos y los votos en listas separadas
+        # Extraer los nombres de los partidos y la cantidad de personas por partido
         partidos = [fila[0] for fila in resultados]
-        votos = [fila[1] for fila in resultados]
+        personas_por_partido = [fila[1] for fila in resultados]
 
         # Crear el gráfico de pastel
         fig, ax = plt.subplots()
-        ax.pie(partidos, labels=votos,
+        ax.pie(personas_por_partido, labels=partidos,
                colors=['mediumslateblue', 'dodgerblue', 'mediumorchid', 'violet'],
                autopct='%1.1f%%',
                wedgeprops={"linewidth": 1, "edgecolor": "white"})
 
         ax.set_title('Elecciones ')
 
-        plt.gcf().patch.set_alpha(0)# 0 para completamente transparente, 1 para opaco
+        # 0 para completamente transparente, 1 para opaco
+        plt.gcf().patch.set_alpha(0)
 
-        plt.tight_layout()#arantizar que los elementos de la trama (como títulos, etiquetas de ejes, etc.)
-                            # estén bien colocados y no se superpongan entre sí.
+        # garantiza que los elementos de la trama (como títulos, etiquetas de ejes, etc.)
+        # estén bien colocados y no se superpongan entre sí.
+        plt.tight_layout()
 
+        #crea un objeto BytesIO, que es un buffer en memoria utilizado para almacenar datos binarios.
         buffer = BytesIO()
+        #guarda el gráfico actual en el buffer de memoria en formato PNG. Especificamos el formato como 'png'
+        # para que se guarde como una imagen PNG.
         plt.savefig(buffer, format='png')
+
+        #mueve el cursor al inicio del buffer, para que podamos leer los datos desde el principio.
         buffer.seek(0)
+
+        # obtienen los datos binarios de la imagen desde el buffer utilizando el método getvalue().
         image_png = buffer.getvalue()
         buffer.close()
 
+        # Codifica la imagen PNG en formato base64 utilizando la función b64encode del módulo base64.
+        # Esto convierte los datos binarios en una cadena base64.
         graphic = base64.b64encode(image_png)
+
+        #decodifica la cadena base64 a una cadena de texto UTF-8. Esto es necesario si deseas utilizar la cadena base64 en un documento HTML.
         graphic = graphic.decode('utf-8')
         print(graphic)
         return graphic
@@ -65,7 +76,15 @@ def registrar_persona(request):
         nombre = request.POST.get('nombre')
         apellido = request.POST.get('apellido')
         partido = request.POST.get('partido')
-        persona = Persona.objects.create(nombre=nombre, apellido=apellido, partido=partido)
-        messages.success(request, 'Persona agregada correctamente.')
+        if partido:
+            # Sí se seleccionó un partido, crear una persona asociada a ese partido
+            Persona.objects.create(nombre=nombre, apellido=apellido, partido=partido)
+            messages.success(request, 'Voto registrado correctamente.')
+        else:
+            # Si no se seleccionó un partido, mostrar un mensaje de error
+            messages.error(request, 'Por favor, elige un partido antes de continuar.')
+            return redirect('fakeit:index')  # Redirigir a la página de inicio o al formulario de registro
+
         return redirect('fakeit:index')
-    return render(request, 'formulario.html')
+
+        return render(request, 'formulario.html')
